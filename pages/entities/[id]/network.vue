f3b578ad08ab5d50f52101ebc94f177b8302b9e8
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-06-18 16:22:52 +0200

    network & data view for individual entity

---
+++ pages/entities/[id]/network.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+definePageMeta({
+	layout: "entity"
+});
+
+const t = useTranslations();
+
+const env = useRuntimeConfig();
+</script>
+
+<template>
+	<template v-if="env.public.NUXT_PUBLIC_DATABASE !== 'disabled'">
+		<ErrorBoundary>
+			<DataNetworkView></DataNetworkView>
+		</ErrorBoundary>
+	</template>
+	<template v-else>
+		<div>{{ t("DataPage.work-in-progress") }}</div>
+	</template>
+</template>
