acad093b50c16aeb7876ff5d397c0a66a765297e
Katharina Wünsche <katharina_wunsche@yahoo.de>
2025-04-15 13:04:23 +0200

    fix: handle "negative" dates (BC)

--- utils/create-date-span.ts
+++ utils/create-date-span.ts
@@ -1,16 +1,36 @@
+function normalizeIsoYear(dateString: string) {
+	return dateString.replace(/^-(\d{1,4})/, (_, year: string) => {
+		return year.padStart(4, "0");
+	});
+}
+
 export function createDateSpan(date: { earliest?: string | null; latest?: string | null }) {
 	const { d } = useI18n();
 
 	const segments: Array<string> = [];
 
 	if (date.earliest != null) {
-		if (date.earliest === date.latest) {
-			return d(date.earliest, { dateStyle: "long" });
+		let dateValue: Date | string = date.earliest;
+		let isBC = false;
+		if (date.earliest.startsWith("-")) {
+			dateValue = new Date(Date.parse(normalizeIsoYear(date.earliest)));
+			dateValue.setFullYear(dateValue.getFullYear() * -1);
+			isBC = true;
 		}
-		segments.push(d(date.earliest, { dateStyle: "medium" }));
+		if (date.earliest === date.latest) {
+			return d(dateValue, { dateStyle: "long" });
+		}
+		segments.push(d(dateValue, { dateStyle: "medium" }) + (isBC ? " BC" : ""));
 	}
 	if (date.latest != null) {
-		segments.push(d(date.latest, { dateStyle: "medium" }));
+		let latestDateValue: Date | string = date.latest;
+		let isBC = false;
+		if (date.latest.startsWith("-")) {
+			latestDateValue = new Date(Date.parse(normalizeIsoYear(date.latest)));
+			latestDateValue.setFullYear(latestDateValue.getFullYear() * -1);
+			isBC = true;
+		}
+		segments.push(d(latestDateValue, { dateStyle: "medium" }) + (isBC ? " BC" : ""));
 	}
 
 	return segments.join(" - ");
