7a345840a0e286b4c28f5e10765964f38f985fe7
Katharina Wünsche <katharina_wunsche@yahoo.de>
2025-05-12 08:56:04 +0200

    chore: update entity-network to new entity-data-graph props

--- components/entity-network.vue
+++ components/entity-network.vue
@@ -5,15 +5,32 @@ const props = defineProps<{
 	networkData: EntityFeature | undefined;
 	id: number;
 }>();
+const depth = ref(1);
+
+const { data: egoNetworkData } = useGetEgoNetworkData(
+	// @ts-expect-error Includes custom, per-instance system classes.
+	computed(() => {
+		return {
+			id: props.id,
+			depth: depth.value,
+			exclude_system_classes: project.network.excludeSystemClasses,
+		};
+	}),
+);
+function updateDepth(newDepth: number) {
+	depth.value = newDepth;
+}
 </script>
 
 <template>
 	<Card class="h-96 overflow-hidden">
 		<VisualisationContainer>
 			<EntityDataGraph
-				v-if="props.networkData != null"
+				v-if="egoNetworkData != null"
 				:id="props.id"
-				:network-data="props.networkData"
+				:current-depth="depth"
+				:network-data="egoNetworkData"
+				@change-depth="(val) => updateDepth(val)"
 			/>
 		</VisualisationContainer>
 	</Card>
